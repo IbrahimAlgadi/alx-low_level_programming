f3e5911ddbc3a64730138b9b6fdde30ad5ca52b2
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-08 11:32:20 -0700

    Commit Fri 08 Sep 2023 11:32:20 AM PDT

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,8 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit < 10; digit++) {
-                printf("%i", digit);
-	}
+	for (digit = 0; digit < 10; digit++)
+        printf("%i", digit);
 	
 	putchar('\n');
 

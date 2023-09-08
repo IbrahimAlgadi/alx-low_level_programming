6d19b904f7a60be3bc438cd283eda0a731c20077
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-08 11:50:27 -0700

    Commit Fri 08 Sep 2023 11:50:27 AM PDT

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,27 @@
  * starting from 0, followed by a new line.
  * Return: 0
  */
+void printCharacter(char c) {
+    putchar(c);
+    if (c != '9')
+    {
+	    putchar(',');
+	    putchar(' ');
+    }
+}
+
 int main(void)
 {
-	int c = 0;
-
-	while (c < 10)
-	{
-		putchar(48 + c);
-		if (c != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		c++;
-	}
+	printCharacter('0');
+	printCharacter('1');
+	printCharacter('2');
+	printCharacter('3');
+	printCharacter('4');
+	printCharacter('5');
+	printCharacter('6');
+	printCharacter('7');
+	printCharacter('8');
+	printCharacter('9');
 	putchar('\n');
 	return (0);
 }

12216fc928020ad3e3db8e9b0a1458218fd2bcde
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-21 20:12:02 +0300

    Fix Line Over 80 Chars

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * _strncpy -  _strncpy is a function that copies characters from src to dest, up to a maximum length of n.
- * It ensures that dest is null-terminated within the specified length and returns a pointer to the modified
- * dest string.
+ * _strncpy -  _strncpy is a function that copies characters from
+ * src to dest, up to a maximum length of n. It ensures that dest
+ * is null-terminated within the specified length and returns a
+ * pointer to the modified dest string.
  * @src: the source string to be copied from.
- * @dest: the destination string where the copied characters will be stored.
- * @n: the maximum number of characters to copy from src to dest.
+ * @dest: the destination string where the copied characters
+ * will be stored.
+ * @n: the maximum number of characters to copy from src to
+ * dest.
  * Return: void
  */
 

b6e5d390e46d77bd30c9f3b51412394361482dd6
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-21 20:10:33 +0300

    Fix Line Over 80 Chars

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp -  is a function that performs a lexicographic comparison of two strings, s1 and s2, and returns an integer
- * value indicating their relationship. It returns 0 if the strings are equal, a negative value if s1 is less than s2,
- * and a positive value if s1 is greater than s2.
+ * _strcmp -  is a function that performs a lexicographic
+ * comparison of two strings, s1 and s2, and returns an
+ * integer value indicating their relationship. It returns
+ * 0 if the strings are equal, a negative value if s1 is
+ * less than s2, and a positive value if s1 is greater than
+ * s2.
  *
  * @s1: the first string to be compared.
  * @s2: the second string to be compared.

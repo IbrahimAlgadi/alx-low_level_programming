dbcff4563b918a9825d6c2c84d0cce7017f603b2
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-21 20:23:48 +0300

    Adding \n at the end of a line

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,18 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
     int i;
 
     i = 0;
-
     while (src[i] != '\0' && i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
+    { dest[i] = src[i]; i++; }
 
     while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
+    { dest[i] = '\0'; i++; }
 
     return (dest);
 }

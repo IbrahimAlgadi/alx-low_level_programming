41e0dc870f077de02b3aa17c0aaade46dc0705b7
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-29 15:13:01 +0300

    Betty Style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,8 @@
  * of x raised to the power of y.
  * Prototype: int _pow_recursion(int x, int y);
  * If y is lower than 0, the function should return -1
- * FYI: The standard library provides a different function: pow. Run man pow to learn more.
+ * FYI: The standard library provides a different function:
+ * pow. Run man pow to learn more.
  *
  * @x: integer
  * @y: integer

eeaf3c5d407cf641ccbab715277aef317b1b0e28
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-09-29 15:12:16 +0300

    Betty Style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,8 @@
  * _strlen_recursion - Write a function that returns the
  * length of a string.
  * Prototype: int _strlen_recursion(char *s);
- * FYI: The standard library provides a similar function: strlen. Run man strlen to learn more.
+ * FYI: The standard library provides a similar function:
+ * strlen. Run man strlen to learn more.
  *
  * @s: pointer to string params
  * Return: recursion

ca961c4d70e5c6e361645303336df80b65e2fc54
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-03 23:10:07 +0300

    Write a function that concatenates all the arguments of your program.
    Prototype: char *argstostr(int ac, char **av);
    Returns NULL if ac == 0 or av == NULL
    Returns a pointer to a new string, or NULL if it fails
    Each argument should be followed by a \n in the new string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - calculate the length of a string.
+ *
+ * @s: the string to calculate it's length.
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+
+/**
+  * argstostr - concatenates all arguments of the program.
+  *
+  * @ac: argument count.
+  * @av: pointer to array of size ac.
+  *
+  * Return: NULL if ac == 0 or av == null, Pointer to new string.
+  * NULL on fail.
+  */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, size = 0, arg_string_idx = 0;
+	char *arg_string;
+
+	while (i < ac)
+	{
+		size = size + _strlen(av[i]) + 1;
+		i++;
+	}
+	/*printf("Total Size Is: %d\n", size);*/
+
+	arg_string = malloc((sizeof(char) * size) + 1);
+	if (arg_string == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (j < _strlen(av[i]))
+		{
+			arg_string[arg_string_idx] = av[i][j];
+			j++;
+			arg_string_idx++;
+		}
+		arg_string[arg_string_idx] = '\n';
+		i++;
+		arg_string_idx++;
+	}
+	/*printf("%d = %d\n", arg_string_idx, size);*/
+	arg_string[size - 1] = '\0';
+
+	return (arg_string);
+}
---
+++ 0x0B-malloc_free/100-run.sh
@@ -0,0 +1,2 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-argstostr.c -o args
+./args.exe I will "show you" how great I am

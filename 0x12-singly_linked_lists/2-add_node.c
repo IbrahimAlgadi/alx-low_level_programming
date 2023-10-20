99c8369ebbd971146542f4141941916c955e4422
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-20 20:59:15 +0300

    Write a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,9 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
+ * add_node - Write a function that adds a new node
+ * at the beginning of a list_t list.
+ *
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *

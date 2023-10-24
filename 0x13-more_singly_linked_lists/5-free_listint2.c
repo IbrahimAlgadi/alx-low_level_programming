52110a38c64a0e25ad51f16e7168dcdb6a33ba00
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-24 13:43:49 +0200

    Write a function that frees a listint_t list.
    Prototype: void free_listint2(listint_t **head);
    The function sets the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Write a function that frees a listint_t list.
+ *
+ * @head: Address of the first node of a list.
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}

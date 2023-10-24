abf29ab545c5bc6f74ff5cf818f044635eb80ae2
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2023-10-24 13:32:53 +0200

    Write a function that prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Write a function that prints all the
+ * elements of a listint_t list.
+ *
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tp;
+	unsigned int cntr = 0;
+
+	tp = h;
+	while (tp)
+	{
+		printf("%d\n", tp->n);
+		cntr++;
+		tp = tp->next;
+	}
+	return (cntr);
+}
